c6dd6215b4433e04062ac7b54ec0a7d011158bea
Secondta <idnssquad300@gmail.com>
2022-01-26 15:43:58 +0700

    first commit

---
+++ library.js
@@ -0,0 +1,36 @@
+/* membuat fungsi */
+// function myFunction() {
+// console.log("Hello World!");
+//   }
+//   myFunction()
+//   ;
+/* fungsi end */
+
+  /* penjumlahan */
+// var x, y;
+// x = 11;
+// y = 5;
+// console.log(x + y);  
+// console.log(x / y);  
+// console.log(x - y);  
+// console.log(x * y);  
+/* penjumlahan end */ 
+
+/* nama */
+// var person = {
+//     firstName: "Dhia",
+//     lastName: "Falah",
+//     age: 16,
+//   };
+//   console.log(person.firstName);
+//   console.log(person.lastName);
+//   console.log(person.age);
+  /* nama end */
+
+  const computer = ["motherboard", "CPU", "LAN Card", "WLAN Card", "Heat Sink", "Power Suppley", "Optical Drive", "Hard Disk", "mouse", "RAM"];
+
+  document.write("ol");
+  for(let i = 0; 1 <computer.length; i++){
+      document.write(`<li>${computer[1]}</li>`);
+  }
+  document.write("</ol>")
